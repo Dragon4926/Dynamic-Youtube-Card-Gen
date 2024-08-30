e0b675b23d0b2a6a73671401a1f40cfc31de16d3
Debopriyo Das <70844598+Dragon4926@users.noreply.github.com>
2024-08-30 20:49:08 +0530

    updated fact calc

--- fact.js
+++ fact.js
@@ -5,3 +5,11 @@ let fact = (n) => {
 document.write(
   `The factorial of the given no is : ${fact(`${prompt(`Enter a no: `)}`)}`
 );
+
+// let fact = (n) => {
+//     let a = Array.from(Array(n+1).keys());
+//     let c = Array.slice(1,).reduce((a, b)=>{
+//         return a*b;
+//     })
+//     return c;
+// } --- using reduce
